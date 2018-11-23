e9a7d6b8a4ffc1412c63bfb2aef93fd7ead7003f
amirkhon <amirkhon_92@mail.ru>
2018-11-23 21:25:28 +0300

    Update sign in

--- website/javascript/templates/Upload.vue
+++ website/javascript/templates/Upload.vue
@@ -13,7 +13,7 @@
             <div class="panel panel-default upload-zone">
                 <div class="panel-body text-center">
                     <h2>Log in to upload bot</h2>
-                    <p style="text-align: center;">In order to play Halite and appear on the leaderboard, you must <a href="./">sign in</a> with your verification code</p>
+                    <p style="text-align: center;">In order to play and appear on the leaderboard, you must <a href="{{baseUrl}}">sign in</a> with your verification code</p>
                 </div>
             </div>
         </div>
